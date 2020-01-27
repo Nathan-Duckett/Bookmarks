9fd19ba4b66c334f0c26ade67458e3127c17d2d2
Nathan Duckett <nduckett13@gmail.com>
2020-01-28 12:42:41 +1300

    Force respond json content

--- Bookmarks/hotlinks.py
+++ Bookmarks/hotlinks.py
@@ -1,31 +1,33 @@
+import logging
 import requests
 
 class Hotlinks:
   
   def __init__(self):
     self.logger = logging.getLogger('bookmarks')
-    self.endpoint = "http://"
+    self.endpoint = "http://localhost"
     
   def get_all(self):
     # Request all links
-    return requests.get(f"{self.endpoint}/get")
+    return requests.get(f"{self.endpoint}/get").json()
   
   def get_link(self, index):
     # Request specific link
-    return requests.get(f"{self.endpoint}/get/{index}")
+    return requests.get(f"{self.endpoint}/get/{index}").json()
     
   def clear_links(self):
     # Request clear all links
-    return requests.get(f"{self.endpoint}/clear")
+    return requests.get(f"{self.endpoint}/clear").json()
     
   def remove_link(self, index):
     # Request delete specific link
-    return requests.get(f"{self.endpoint}/remove/{index}")
+    return requests.get(f"{self.endpoint}/remove/{index}").json()
     
   def add_link(self, link_addr):
     # Request add link
     body = {
       "link": link_addr
     }
-    requests.post(f"{self.endpoint}/add", data=body)
+    res = requests.post(f"{self.endpoint}/add", data=body)
+    return res.status_code == 200
     
